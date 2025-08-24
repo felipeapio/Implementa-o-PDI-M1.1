1683d835bfab2655dc53f13c3395944305e20d3b
felipe apio de souza <felipeapiodesouza@hotmail.com>
2025-08-24 12:27:38 -0300

    realizando a primeira parte do trabalho, que consiste em pegar uma imagem colorida e pegar pixel a pixel para tornamos esse pixel cinza e consequentemente a imagem final se tornar cinza, e salvarmos essa nova imagem

--- src/grayscale.cpp
+++ src/grayscale.cpp
@@ -6,14 +6,36 @@ using namespace std; // Usado para evitar escrever "std::" antes de elementos da
 using namespace cv;  // Usado para evitar escrever "cv::" antes de elementos da biblioteca OpenCV
 
 int main()
-{ // Função principal, onde o programa começa a execução
-    // Código do seu programa vai aqui
-    cout << "Ola, Mundo!" << endl;                      // Exibe uma mensagem na tela
+{
+    // atividade 1 Conversão para Tons de Cinza
     Mat image = imread("../image/imagem_original.jpg"); // Lê a imagem do arquivo "image.jpg"
     if (image.empty())
     {
         cerr << "Erro ao carregar a imagem!" << endl;
         return 1;
     }
+
+    // Cria uma nova imagem em tons de cinza com o mesmo tamanho, mas 1 canal
+    Mat grayImage(image.rows, image.cols, CV_8UC1);
+
+    // Aqui vamos percorre cada pixel (linha e coluna obviamente) da imagem original
+    for (int i = 0; i < image.rows; i++)
+    {
+        for (int j = 0; j < image.cols; j++)
+        {
+            Vec3b pixel = image.at<Vec3b>(i, j); // Acessa o pixel (B, G, R)
+            uchar blue = pixel[0];
+            uchar green = pixel[1];
+            uchar red = pixel[2];
+
+            // Aplica a fórmula de conversão para tons de cinza
+            uchar gray = static_cast<uchar>(0.299 * red + 0.587 * green + 0.114 * blue);
+
+            // Define o valor na imagem em tons de cinza
+            grayImage.at<uchar>(i, j) = gray;
+        }
+    }
+    imwrite("../image/imagem_cinza.jpg", grayImage); // salvando a nova imagem ao qual alteramos para cinza
+
     return 0; // Retorna 0, indicando que o programa terminou com sucesso
 }
